ad5f54157f3678eb639e36625042fc05957f4cf8
Grigory Zotkevich <zotkevich@bk.ru>
2020-08-15 17:19:17 +0700

    Add XSS protection

--- src/js/components/NewsCard.js
+++ src/js/components/NewsCard.js
@@ -1,17 +1,18 @@
 import { transformDate } from '../utils/transofrm-date.js';
 import { filterText } from '../utils/filter-text.js';
+import { sanitizeHTML} from '../utils/sanitize-html.js';
 
 export class NewsCard {
   constructor() {}
   create(data) {
     const markup = `
       <li class="search-results__card">
-        <a class="search-results__card-link" target="_blank" href="${data.url}"></a>
-        <img src="${data.urlToImage}" alt="" class="search-results__card-image">
-        <p class="search-results__card-date">${transformDate(data.publishedAt)}</p>
-        <h3 class="search-results__card-title">${data.title}</h3>
-        <p class="search-results__card-text">${filterText(data.description)}</p>
-        <p class="search-results__author">${data.source.name}</p>
+        <a class="search-results__card-link" target="_blank" href="${sanitizeHTML(data.url)}"></a>
+        <img src="${sanitizeHTML(data.urlToImage)}" alt="" class="search-results__card-image">
+        <p class="search-results__card-date">${sanitizeHTML(transformDate(data.publishedAt))}</p>
+        <h3 class="search-results__card-title">${sanitizeHTML(data.title)}</h3>
+        <p class="search-results__card-text">${sanitizeHTML(filterText(data.description))}</p>
+        <p class="search-results__author">${sanitizeHTML(data.source.name)}</p>
       </li>
     `
     const element = document.createElement('div');
